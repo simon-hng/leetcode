6c7bd96c3d038e2fd0e979cede73956bf765b610
Simon Huang <simon.huang@tum.de>
2022-11-01 22:31:31 +0100

    fix naive approach

--- 69-sqrtx.py
+++ 69-sqrtx.py
@@ -1,9 +1,17 @@
+import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        for i in range(x):
-            if (i * i >= x): return i
+        for i in range(x + 1, 0, -1):
+            if (i * i <= x): return (i)
         return 0
 
 sol = Solution()
-print([(sol.mySqrt(x), x) for x in range(10)])
         
+n = 1000
+a = list(map(
+    lambda x,y: x == y,
+    [sol.mySqrt(x) for x in range(n)],
+    [math.floor(math.sqrt(x)) for x in range(n)]
+))
+
+print("all result from 0 to " + str(n) + " are equal: " + str(all(a)))
