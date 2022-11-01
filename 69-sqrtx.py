861bc5c3a64381ebbcf57e48b21800bcf853397a
Simon Huang <simon.huang@tum.de>
2022-11-01 22:47:01 +0100

    add binary search

--- 69-sqrtx.py
+++ 69-sqrtx.py
@@ -1,13 +1,23 @@
 import math
 class Solution:
-    def mySqrt(self, x: int) -> int:
+    def naiveSqrt(self, x: int) -> int:
         for i in range(x + 1, 0, -1):
             if (i * i <= x): return (i)
         return 0
 
+    def mySqrt(self, x: int) -> int:
+        low = 1; high = x
+
+        while True:
+            m = (low + high) // 2
+            if m * m <= x < (m + 1) * (m + 1): return m
+
+            if m * m > x: high = m
+            else: low = m
+
 sol = Solution()
         
-n = 1000
+n = 1000 
 a = list(map(
     lambda x,y: x == y,
     [sol.mySqrt(x) for x in range(n)],
