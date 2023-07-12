2f8c2597de1b7032daa89484117b47b25c85708c
Simon Huang <simon.huang@tum.de>
2023-07-13 00:08:03 +0200

    fix(03-longest-substring-without-repeating-characters.py): improve code readability by adding line breaks and indentation feat(03-longest-substring-without-repeating-characters.py): add support for finding the longest substring without repeating characters feat(28-find-the-index-of-the-first-occurrence-in-a-string.py): add implementation for finding the index of the first occurrence of a substring in a string feat(longestHeterogramicSubstring.py): add implementation for finding the longest heterogramic substring in a string test(longestHeterogramicSubstring.py): add test cases to verify the correctness of the implementation

--- 03-longest-substring-without-repeating-characters.py
+++ 03-longest-substring-without-repeating-characters.py
@@ -1,22 +1,26 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if (len(s) == 0): return 0
-        
+        if (len(s) == 0):
+            return 0
+
         contained = set()
         result = 0
 
         for element in s:
-            if (element in contained): break;
+            if (element in contained):
+                break
 
-            result += 1;
+            result += 1
             contained.add(element)
 
         return max(self.lengthOfLongestSubstring(s[1:]), result)
 
+
 def main():
-    sol =  Solution();
-    print(sol.lengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+    sol = Solution()
+    print(sol.lengthOfLongestSubstring(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+
 
 if (__name__ == "__main__"):
     main()
-
