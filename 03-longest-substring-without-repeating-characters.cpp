0b2294eca400b2e06c6e45be5e4d715eb40fe00f
Simon Huang <simon.huang@tum.de>
2023-07-15 01:26:44 +0200

    chore(.clangd): add .clangd configuration file to specify compile flags for C++ code feat(03-longest-substring-without-repeating-characters.cpp): add solution for finding the length of the longest substring without repeating characters

---
+++ 03-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(std::string s) {
+    if (s.length() == 0) {
+      return 0;
+    }
+
+    std::unordered_map<char, int> map;
+    int max = 0;
+
+    for (int i = 0, j = 0; i < s.length(); ++i) {
+      if (map.find(s[i]) != map.end()) {
+        j = std::max(j, map.find(s[i])->second + 1);
+      }
+      map.insert_or_assign(s[i], i);
+      max = std::max(max, i - j + 1);
+    }
+
+    return max;
+  }
+};
+
+int main() {
+  Solution solution;
+  std::cout << solution.lengthOfLongestSubstring("abcabcbb") << std::endl;
+}
