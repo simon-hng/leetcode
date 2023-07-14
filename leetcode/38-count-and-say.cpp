05fc37b1198cb76e0841e4b2a2aef433a813d1f1
Simon Huang <simon.huang@tum.de>
2023-07-15 01:50:59 +0200

    chore: move into subfolder

---
+++ leetcode/38-count-and-say.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iostream>
+
+class Solution {
+ public:
+  std::string countAndSay(int n) {
+    if (n == 1) {
+      return "1";
+    }
+
+    int numDigits = std::log10(n + 1);
+    for (int i = 0; i < numDigits; i++) {
+      int digit = n / (numDigits - i);
+    };
+
+    return "0";
+  }
+};
